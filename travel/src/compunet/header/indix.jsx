7879f710521bc7f8159b4cb04eebd248dc238c30
Fakhre Alam <fakhrebuneri@gmail.com>
2024-11-05 18:39:17 +0500

    Update indix.jsx

--- travel/src/compunet/header/indix.jsx
+++ travel/src/compunet/header/indix.jsx
@@ -3,8 +3,7 @@ import "./style.css";
 import { RiIndentDecrease } from "@remixicon/react";
 import { useState } from "react";
 import { RiCloseLine} from "@remixicon/react";
-// this is <header></header>
-//this is a header section
+
 
 function Navbar() {
   const [isNavActive, setIsNavActive] = useState(false);
